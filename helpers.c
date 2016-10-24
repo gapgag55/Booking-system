cb79654fbb7320be9dbe7c4af9b34134bce80b54
sarayut lawilai <zxc5546@hotmail.com>
2016-10-24 20:15:32 +0700

    Update Booking

--- helpers.c
+++ helpers.c
@@ -68,3 +68,34 @@ char *getMonth(int order) {
     return month;
 }
 
+int getMonthInt(char m[]) {
+    
+    if(strcmp(m,"Jan") == 0) {
+        return 0;
+    } else if(strcmp(m,"Feb") == 0) {
+        return 1;
+    } else if(strcmp(m,"Mar") == 0) {
+        return 2;
+    } else if(strcmp(m,"Api") == 0) {
+        return 3;
+    } else if(strcmp(m,"May") == 0) {
+        return 4;
+    } else if(strcmp(m,"Jun") == 0) {
+        return 5;
+    } else if(strcmp(m,"July") == 0) {
+        return 6;
+    } else if(strcmp(m,"Aug") == 0) {
+        return 7;
+    } else if(strcmp(m,"Sep") == 0) {
+        return 8;
+    } else if(strcmp(m,"Oct") == 0) {
+        return 9;
+    } else if(strcmp(m,"Nov") == 0) {
+        return 10;
+    } else {
+        return 11;
+    }
+    
+    return 0;
+}
+
--- page-room.c
+++ page-room.c
@@ -1,7 +1,156 @@
 #include <stdio.h>
+#include <time.h>
+
+int getCalendar(char m[]) {
+    
+    char days[7][4] = {"Sun","Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    int month = getMonthInt(m);
+    
+    int first_s = 2;
+    int start = first_s;
+    int maxDay = 29;
+    int countDay = 1;
+    int roomDay = 1;
+    
+    int column = 7;
+    int subRow = 7;
+    int row = (maxDay + start)/ 7;
+    if(row > 5) row = 6;
+    else row = 5;
+    
+    
+    /*======= HEAD =======*/
+    int a,b,c,d;
+    for (a = 0; a < 3; a++) {
+        for (b = 0; b < 7; b++) {
+            if (a == 0 || a == 2) {
+                if (b == 0) {
+                    printf("|");
+                    for(c = 0; c < 4; c++) {
+                        printf("-");
+                    }
+                } else {
+                    for(c = 0; c < 15; c++) {
+                        printf("-");
+                    }
+                }
+            } else {
+                if (b == 0) {
+                    printf("| %2s", days[b]);
+                } else {
+                    printf("%9s      ", days[b]);
+                }
+            }
+            printf("|");
+        }
+        printf("\n");
+    }
+    
+    
+    // DAYS
+    for(a = 0; a < row; a++) {
+        for(b = 0; b < subRow; b++) {
+            if(a == 0) {
+                start = first_s;
+                roomDay = 1;
+            } else {
+                roomDay += 1;
+            }
+            for(c = 0; c < column; c ++) {
+                    if(start == 0) {
+                        if(b == 0) {
+                            if(c == 0) {
+                                if(countDay <= maxDay) {
+                                    printf("| %2d ", countDay);
+                                } else {
+                                    printf("|    ");
+                                }
+                            } else {
+                                if(countDay <= maxDay) {
+                                    printf("%9d      ", countDay);
+                                } else {
+                                    printf("               ");
+                                }
+                            }
+                            ++countDay;
+                        } else if(b < 6) {
+                            if(c == 0) {
+                                printf("|    ");
+                            } else {
+                                
+                                // EDIT HERE
+                                if(roomDay <= maxDay) {
+                                    printf("IT101 %2d(M,A)  ", roomDay);
+                                } else {
+                                    printf("               ");
+                                }
+                                ++roomDay;
+                            }
+                        } else {
+                            if(c == 0) {
+                                printf("|");
+                                for(d = 0; d < 4; d++) {
+                                    printf("-");
+                                }
+                            } else {
+                                for(d = 0; d < 15; d++) {
+                                    printf("-");
+                                }
+                            }
+                            printf("|");
+                        }
+                    } else {
+                        if(b == 6) {
+                            if(c == 0) {
+                                printf("|");
+                                for(d = 0; d < 4; d++) {
+                                    printf("-");
+                                }
+                            } else {
+                                for(d = 0; d < 15; d++) {
+                                    printf("-");
+                                }
+                            }
+                            printf("|");
+                        } else {
+                            if(c == 0) {
+                                printf("|    ");
+                            } else {
+                                printf("               ");
+                            }
+                        }
+                    }
+                    --start;
+                    if(start < 1) start = 0;
+                    if(b != 6) printf("|");
+            }
+            roomDay = countDay - 7;
+            if(b != 6 ) printf("\n");
+        }
+        printf("\n");
+    }
+    
+    
+    return 0;
+}
+
 
 int roomAbilityPage() {
     
-    printf("Room Ability page");
+    char month[3];
+    
+    printf("Room Ability page\n");
+//    
+//    time_t t = time(NULL);
+//    struct tm tm = *localtime(&t);
+//    
+//    printf("now: %d-%d-%d %d:%d:%d\n", tm.tm_wday, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+//
+    
+//    printf("Select a month: ");
+//    scanf("%s", &month);
+    
+    getCalendar(month);
+    
     return 0;
 }
