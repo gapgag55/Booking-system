ca4d4be91c005948be2b2ad746a963f115ec97eb
tanakitti <tanakit_21235@hotmail.com>
2016-11-06 23:04:53 +0700

    bank-6-11-2559

--- helpers.c
+++ helpers.c
@@ -1,5 +1,5 @@
 char menus(char lower, char upper, char text[]) {
-    
+
     printf("\tEnter %c or %c for\t%s\n", lower, upper, text);
     return 0;
 }
@@ -7,11 +7,11 @@ char menus(char lower, char upper, char text[]) {
 int numlen(int num) {
     int count = 0;
     int mod;
-    
+
     while(num > 0) {
         num = num / 10;
         mod = num % 1;
-        
+
         num = num - mod;
         ++count;
     }
@@ -20,7 +20,7 @@ int numlen(int num) {
 
 char *getMonth(int order) {
     char *month;
-    
+
     switch (order) {
         case 0:
             month = "Jan";
@@ -62,38 +62,38 @@ char *getMonth(int order) {
             month = "Error";
             break;
     }
-    
+
     return month;
 }
 
 // getMonthInt("Nov");
 
 int getMonthInt(char m[]) {
-    
+
     if(strcmp(m,"Jan") == 0) {
-        return 1;
+        return 0;
     } else if(strcmp(m,"Feb") == 0) {
-        return 2;
+        return 1;
     } else if(strcmp(m,"Mar") == 0) {
-        return 3;
+        return 2;
     } else if(strcmp(m,"Api") == 0) {
-        return 4;
+        return 3;
     } else if(strcmp(m,"May") == 0) {
-        return 5;
+        return 4;
     } else if(strcmp(m,"Jun") == 0) {
-        return 6;
+        return 5;
     } else if(strcmp(m,"July") == 0) {
-        return 7;
+        return 6;
     } else if(strcmp(m,"Aug") == 0) {
-        return 8;
+        return 7;
     } else if(strcmp(m,"Sep") == 0) {
-        return 9;
+        return 8;
     } else if(strcmp(m,"Oct") == 0) {
-        return 10;
+        return 9;
     } else if(strcmp(m,"Nov") == 0) {
-        return 11;
+        return 10;
     } else if(strcmp(m, "Dec") == 0){
-        return 12;
+        return 11;
     } else {
         return -1;
     }
@@ -101,7 +101,7 @@ int getMonthInt(char m[]) {
 
 int getRoom(int id) {
     int room;
-    
+
     switch (id) {
         case 1:
             room = 101;
@@ -119,6 +119,37 @@ int getRoom(int id) {
             room = 105;
             break;
     }
-    
+
     return room;
+}
+int printMonth(char m[])
+{
+    if(strcmp(m,"Jan") == 0) {
+        printf("The monthly view of all meeting room on January 2016\n");
+    } else if(strcmp(m,"Feb") == 0) {
+        printf("The monthly view of all meeting room on February 2016\n");
+    } else if(strcmp(m,"Mar") == 0) {
+        printf("The monthly view of all meeting room on March 2016\n");
+    } else if(strcmp(m,"Api") == 0) {
+        printf("The monthly view of all meeting room on April 2016\n");
+    } else if(strcmp(m,"May") == 0) {
+        printf("The monthly view of all meeting room on May 2016\n");
+    } else if(strcmp(m,"Jun") == 0) {
+        printf("The monthly view of all meeting room on June 2016\n");
+    } else if(strcmp(m,"July") == 0) {
+        printf("The monthly view of all meeting room on July 2016\n");
+    } else if(strcmp(m,"Aug") == 0) {
+        printf("The monthly view of all meeting room on August 2016\n");
+    } else if(strcmp(m,"Sep") == 0) {
+        printf("The monthly view of all meeting room on September 2016");
+    } else if(strcmp(m,"Oct") == 0) {
+        printf("The monthly view of all meeting room on October 2016\n");
+    } else if(strcmp(m,"Nov") == 0) {
+        printf("The monthly view of all meeting room on November 2016\n");
+    } else if(strcmp(m, "Dec") == 0){
+        printf("The monthly view of all meeting room on December\n");
+    } else {
+        return 0;
+    }
+    return 1;
 }
